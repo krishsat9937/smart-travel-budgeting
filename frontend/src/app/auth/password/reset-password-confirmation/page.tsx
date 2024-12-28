159b61b2098084c4c1015be7ba786dd8432ebf24
Krishna Kumar <krishsat9937@gmail.com>
2024-12-28 23:58:02 +0100

    fix deployment

--- frontend/src/app/auth/password/reset-password-confirmation/page.tsx
+++ frontend/src/app/auth/password/reset-password-confirmation/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+
 import ResetPasswordConfirmation from "@/app/components/ResetPasswordConfirmation";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main>
+      <Suspense fallback={<div>Loading...</div>}>
       <ResetPasswordConfirmation />
+      </Suspense>
     </main>
   );
 }
