3023ccfefb000c14e9282d6ff066508931a2cd90
Krishna Kumar <krishsat9937@gmail.com>
2024-12-31 00:51:10 +0100

    fix db changes

--- ApiRoot/settings.py
+++ ApiRoot/settings.py
@@ -87,17 +87,17 @@ WSGI_APPLICATION = "ApiRoot.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default='postgresql://travel_budgeter_user:hCfy8a3FU6c0QMouPtBQDHrAHrykOsPL@dpg-ctpihga3esus73dic2t0-a.frankfurt-postgres.render.com/travel_budgeter'
-#     )
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://travel_budgeter_user:hCfy8a3FU6c0QMouPtBQDHrAHrykOsPL@dpg-ctpihga3esus73dic2t0-a.frankfurt-postgres.render.com/travel_budgeter'
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
