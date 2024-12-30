5ebf048f446294280bf635c36dd38b0292d99e86
Krishna Kumar <krishsat9937@gmail.com>
2024-12-30 23:59:14 +0100

    set debug to true

--- ApiRoot/settings.py
+++ ApiRoot/settings.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = "django-insecure-us)-)2ol=f725b_aq+#!2b)bupxd58y67ir3!=-q663vb_moyy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = [
 #     'https://travel-budgeter-frontend.vercel.app',
